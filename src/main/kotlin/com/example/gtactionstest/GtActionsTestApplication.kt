fde5f693e0ad6a847f0f57235ba524bf49133d3d
ank <ank@anadeainc.com>
2023-10-14 23:40:49 +0300

    Add project

---
+++ src/main/kotlin/com/example/gtactionstest/GtActionsTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gtactionstest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GtActionsTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<GtActionsTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/gtactionstest/GtActionsTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gtactionstest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GtActionsTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
