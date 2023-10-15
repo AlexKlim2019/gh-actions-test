a99307a16bf4d1e5ce195af1efdd95e8b6a864b3
ank <ank@anadeainc.com>
2023-10-15 11:16:03 +0300

    edited prepare_local.sh

--- scripts/prepare_local.sh
+++ scripts/prepare_local.sh
@@ -2,5 +2,5 @@
 echo ">>> Enter ip:" && read ip &&
 echo ">>> Public ssh key is being added on ip address $ip..." && 
 cat C:/Users/klyme/.ssh/id_ed25519.pub | ssh -i "gh_actions.pem" ubuntu@$ip "sudo sshcommand acl-add dokku admin" &&
-echo ">>> New url for dokku branch is being set..." && git remote add dokku dokku@$ip:gha-test &&
+echo ">>> New url for dokku branch is being set..." && git remote set-url dokku dokku@$ip:gha-test &&
 ssh dokku@$ip config:set gha-test SPRING_PROFILES_ACTIVE=dev PORT=8080
